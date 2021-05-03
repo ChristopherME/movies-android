f6f90de4947e0126718430675ae6a80db6ed8aeb
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-03 17:13:25 -0500

    render MovieListFragment.kt ui

--- features/movies/src/main/java/com/christopher_elias/movies/presentation/ui/movies_list/MovieListFragment.kt
+++ features/movies/src/main/java/com/christopher_elias/movies/presentation/ui/movies_list/MovieListFragment.kt
@@ -2,9 +2,16 @@ package com.christopher_elias.movies.presentation.ui.movies_list
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.christopher_elias.movies.R
+import com.christopher_elias.movies.databinding.FragmentMovieListBinding
+import com.christopher_elias.movies.presentation.model.MovieUi
+import com.christopher_elias.movies.presentation.ui.movies_list.adapter.MovieListAdapter
+import com.christopher_elias.utils.consumeOnce
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -20,16 +27,54 @@ import timber.log.Timber
 
 class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
 
+    private val binding by viewBinding(FragmentMovieListBinding::bind)
+
     private val moviesViewModel: MovieListViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+        collectUiState()
+    }
 
+    private fun initView() {
+        binding.rvMovies.adapter = MovieListAdapter(clickListener = ::onMovieClicked)
+    }
+
+    private fun collectUiState() {
         viewLifecycleOwner.lifecycleScope.launch {
             moviesViewModel.uiState.collect { state ->
-                Timber.d("State: $state")
+                renderUiState(state)
             }
         }
+    }
 
+    private fun renderUiState(state: MovieListUiState) {
+        with(state) {
+            // Progress
+            binding.progressBarMovies.isVisible = isLoading
+
+            // Bind movies.
+            (binding.rvMovies.adapter as MovieListAdapter)
+                .submitList(movies)
+
+            // Empty view
+            binding.tvMoviesEmpty.isVisible = movies.isEmpty()
+
+            // Display error if any. Only once.
+            error?.let {
+                it.consumeOnce { failure ->
+                    Toast.makeText(
+                        requireContext(),
+                        "$failure",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun onMovieClicked(movie: MovieUi) {
+        Timber.d("Movie: $movie")
     }
 }
