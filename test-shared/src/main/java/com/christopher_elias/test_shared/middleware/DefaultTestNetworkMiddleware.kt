b5ab81d2fee20057a37ee9b1ecd15f85593122fa
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-03 10:33:12 -0500

    changes: add :core:network module to :test-shared module in order to provide some defaults testings utils.

---
+++ test-shared/src/main/java/com/christopher_elias/test_shared/middleware/DefaultTestNetworkMiddleware.kt
@@ -0,0 +1,24 @@
+package com.christopher_elias.test_shared.middleware
+
+import com.christopher_elias.network.middleware.NetworkMiddleware
+import com.christopher_elias.network.models.exception.NetworkMiddlewareFailure
+
+/*
+ * Created by Christopher Elias on 3/05/2021
+ * christopher.mike.96@gmail.com
+ *
+ * Loop Ideas
+ * Lima, Peru.
+ */
+
+class DefaultTestNetworkMiddleware(
+    private val isMiddlewareValid: Boolean,
+    private val failureMessage: String = ""
+) : NetworkMiddleware() {
+
+    override val failure: NetworkMiddlewareFailure
+        get() = NetworkMiddlewareFailure(middleWareExceptionMessage = failureMessage)
+
+    override fun isValid(): Boolean = isMiddlewareValid
+
+}
