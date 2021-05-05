7e534600ae90142fc83779d0ed45cad07196257d
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-05 15:54:49 -0500

    add jetpack Navigation component to app & movies modules.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -36,6 +36,7 @@ object Versions {
 
     const val FRAGMENT_VERSION = "1.3.3"
     const val LIFECYCLE_VERSION = "2.3.1"
+    const val JETPACK_NAVIGATION_VERSION = "2.3.5"
 
     const val RETROFIT_VERSION = "2.9.0"
     const val LOGGING_INTERCEPTOR_VERSION = "4.9.0"
@@ -56,7 +57,6 @@ object Versions {
     const val ANDROIDX_CONSTRAINT_LAYOUT_VERSION = "2.0.4"
 
     const val DI_KOIN_VERSION = "3.0.1"
-
 
 }
 
@@ -98,6 +98,11 @@ object Libs {
     // Fragments
     val fragments = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_VERSION}"
     val viewModels = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE_VERSION}"
+
+    //Navigation
+    val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.JETPACK_NAVIGATION_VERSION}"
+    val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.JETPACK_NAVIGATION_VERSION}"
+    val navigationSafeArgsPlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.JETPACK_NAVIGATION_VERSION}"
 
     // ||
     // TESTING
