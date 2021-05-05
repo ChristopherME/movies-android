b0a1dbb653233b57c6d786563eef7e5792f9bba0
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-05 16:10:16 -0500

    integrate :features:movies nav graph xml into app module

--- app/src/main/java/com/christopher_elias/movies/ui/MainActivity.kt
+++ app/src/main/java/com/christopher_elias/movies/ui/MainActivity.kt
@@ -3,14 +3,10 @@ package com.christopher_elias.movies.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.christopher_elias.movies.R
-import com.christopher_elias.features.movies.presentation.ui.movies_list.MovieListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainContainerView, MovieListFragment())
-            .commit()
     }
 }
