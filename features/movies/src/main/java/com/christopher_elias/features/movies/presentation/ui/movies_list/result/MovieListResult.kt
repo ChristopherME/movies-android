c9fce3e7882d18fdcc3a1336da86d4dff996f929
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-14 11:51:00 -0500

    implementing MovieListProcessorHolder.kt

---
+++ features/movies/src/main/java/com/christopher_elias/features/movies/presentation/ui/movies_list/result/MovieListResult.kt
@@ -0,0 +1,19 @@
+package com.christopher_elias.features.movies.presentation.ui.movies_list.result
+
+import com.christopher_elias.common.models.presentation.MovieUi
+import com.christopher_elias.features.movies.mvi_core.MviResult
+import com.christopher_elias.functional_programming.Failure
+
+/*
+ * Created by Christopher Elias on 14/05/2021
+ * christopher.mike.96@gmail.com
+ *
+ * Loop Ideas
+ * Lima, Peru.
+ */
+
+sealed class MovieListResult : MviResult {
+    data class Success(val movies: List<MovieUi>) : MovieListResult()
+    data class Error(val failure: Failure) : MovieListResult()
+    object Loading : MovieListResult()
+}
