73c829ac2ec55e4063bc7c995ff4120e49caa947
Christopher Elias <christopher.mike.96@gmail.com>
2021-05-18 18:00:33 -0500

    breaking changes:  - upgrade kotlin version  - upgrade Koin version features:  - add fragment unit test dependency

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -35,9 +35,9 @@ object Common {
 
 object Versions {
 
-    const val KOTLIN_VERSION = "1.4.32"
-    const val COROUTINES_VERSION = "1.4.3"
-    const val GRADLE_TOOLS_VERSION = "4.2.0"
+    const val KOTLIN_VERSION = "1.5.0"
+    const val COROUTINES_VERSION = "1.5.0"
+    const val GRADLE_TOOLS_VERSION = "4.2.1"
 
     const val FRAGMENT_VERSION = "1.3.3"
     const val LIFECYCLE_VERSION = "2.3.1"
@@ -61,8 +61,9 @@ object Versions {
     const val GOOGLE_MATERIAL_VERSION = "1.3.0"
     const val ANDROIDX_CONSTRAINT_LAYOUT_VERSION = "2.0.4"
     const val ANDROIDX_SWIPE_REFRESH_LAYOUT_VERSION = "1.1.0"
+    const val ANDROIDX_FRAGMENT_TESTING_VERSION = "1.3.3"
 
-    const val DI_KOIN_VERSION = "3.0.1"
+    const val DI_KOIN_VERSION = "3.0.2"
 
 }
 
@@ -100,6 +101,7 @@ object Libs {
 
     // DI Koin
     val koinAndroidx = "io.insert-koin:koin-android:${Versions.DI_KOIN_VERSION}"
+    val koinJvmTest = "io.insert-koin:koin-test-junit4:${Versions.DI_KOIN_VERSION}"
 
     // Fragments
     val fragments = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_VERSION}"
@@ -129,5 +131,8 @@ object Libs {
 
     // Espresso
     val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+
+    // Fragments
+    val fragmentTest = "androidx.fragment:fragment-testing:${Versions.ANDROIDX_FRAGMENT_TESTING_VERSION}"
 
 }
